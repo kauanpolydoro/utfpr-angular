5be66187845127e97711509e523841fc856e9ec3
Kauan Polydoro <kauan.2812@hotmail.com>
2023-04-29 10:43:24 -0300

    Refatorado testes unitários do Cliente

--- angular-container/src/app/models/cliente.spec.ts
+++ angular-container/src/app/models/cliente.spec.ts
@@ -1,7 +1,20 @@
 import { Cliente } from './cliente';
 
 describe('Cliente', () => {
+
+  let cliente: Cliente;
+
+  beforeEach(() => {
+    cliente = new Cliente(
+      "Kauan Polydoro",
+      1000,
+      new Date("2023-04-29T10:38:00"),
+    );
+  });
+
   it('should create an instance', () => {
-    expect(new Cliente()).toBeTruthy();
+
+    expect(cliente).toBeTruthy();
+
   });
 });
