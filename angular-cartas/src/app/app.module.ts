5c11527796a11815702ed01a5a7a52b6754bf6ff
Kauan Polydoro <kauan.2812@hotmail.com>
2023-05-08 16:15:36 -0300

    wip

--- angular-cartas/src/app/app.module.ts
+++ angular-cartas/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { SuitButtonComponent } from './components/suit-button/suit-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    SuitButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-cartas/src/app/components/suit-button/suit-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suit-button',
+  templateUrl: './suit-button.component.html',
+  styleUrls: ['./suit-button.component.css']
+})
+export class SuitButtonComponent {
+
+}
